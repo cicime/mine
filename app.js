69b1e35737a5f8b378dd313cc1011c01e345c4ce
cicime <yuanming200755@163.com>
2018-09-17 21:46:47 +0800

    :rose:

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 const app = express();
 
-
+app.set('view engine', 'ejs')
+app.set('views', './views')
 app.use(express.static('./static'))
+
+
+app.get('/', (req, res) => {
+  
+  res.render('index')
+})
 
 
 app.listen(80, function (error) {
