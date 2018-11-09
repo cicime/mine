39436108a5bb942b79a273d6cafb1e4d1abe5313
cicime <yuanming200755@163.com>
2018-11-09 15:15:16 +0800

    :art:

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
-
 const app = express();
-const ENV = process.env.NODE_ENV;
+
+const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.NODE_PORT || 8088;
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
@@ -9,11 +10,15 @@ app.use(express.static('./static'))
 
 
 app.get('/', (req, res) => {
-
   res.render('index', { ENV });
 })
 
 
-app.listen(80, function (error) {
-  console.log('runing 80')
+app.listen(PORT, function (error) {
+  if (error) {
+    console.log('ರೃ∀ರೃ 报错了！！！', error);
+    return
+  }
+
+  console.log(`APP RUNNING ✧(≖ ◡ ≖✿  — ${ENV} - ${PORT}`);
 })
